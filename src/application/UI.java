6c84949999282efa84c3ee8304145c1e3be03e02
Everton Andre <evertonandre1996@gmail.com>
2021-09-13 09:39:23 -0300

    Knight

--- src/application/UI.java
+++ src/application/UI.java
@@ -35,7 +35,7 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	// limpar tela / stackoverflow
 	public static void clearScreen() {
-		System.out.print( "\033[H\033[2J]");
+		System.out.print( "\033[H\033[2J");
 		System.out.flush();
 	}
 	
